e1b3693d282661159ad141a70a6f62d68a97c8b1
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-11-02 13:06:06 +0100

    Method rename

--- spotify_get/spotify_get.py
+++ spotify_get/spotify_get.py
@@ -29,7 +29,7 @@ def get_input():
     return ids
 
 
-def trunc_artist_list(artist_list):
+def join_artist_list(artist_list):
     output = []
     for artist in artist_list:
         output.append(artist['name'])
@@ -40,7 +40,7 @@ def get_track_info(track_id):
     response = urllib.request.urlopen('https://api.spotify.com/v1/tracks/{}'.format(track_id)).read().decode('utf-8')
     response = json.loads(response)
     track_title = response['name']
-    artists = trunc_artist_list(response['artists'])
+    artists = join_artist_list(response['artists'])
     return ' | '.join([track_title, artists])
 
 
@@ -48,7 +48,7 @@ def get_album_info(album_id):
     response = urllib.request.urlopen('https://api.spotify.com/v1/albums/{}'.format(album_id)).read().decode('utf-8')
     response = json.loads(response)
     album_name = response['name']
-    artists = trunc_artist_list(response['artists'])
+    artists = join_artist_list(response['artists'])
     return ' | '.join([album_name, artists])
 
 
