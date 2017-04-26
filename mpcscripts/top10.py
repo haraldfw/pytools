dfeaec1b754a7eb9ebe54f11e30d971d27fda3bc
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-04-26 12:56:56 +0200

    attempted fix of top10

--- mpcscripts/top10.py
+++ mpcscripts/top10.py
@@ -4,5 +4,8 @@ import sys
 artists = sys.argv[1:]
 for artist in artists:
     artist = str(artist).rstrip('\n')
-    subprocess.Popen("mpc search artist '{}' | grep track | head -10 | mpc add".format(artist),
-                     shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    cmd = "mpc -f '%file% %artist%' search artist '{0}' | grep track | grep '{0}$' | head -10"
+    lines = subprocess.check_output(cmd.format(artist))
+    for line in lines:
+        subprocess.Popen("mpc add {}".format(line),
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
