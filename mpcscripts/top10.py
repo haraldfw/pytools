05c6a58c4f115ee44a9bfc6ee8baefca7d4df94e
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-04-26 13:03:29 +0200

    py plz

--- mpcscripts/top10.py
+++ mpcscripts/top10.py
@@ -5,7 +5,7 @@ artists = sys.argv[1:]
 for artist in artists:
     artist = str(artist).rstrip('\n')
     cmd = "mpc -f '%file% %artist%' search artist '{0}' | grep track | grep '{0}$' | head -10"
-    result = subprocess.run([cmd.format(artist)], stdout=subprocess.PIPE)
-    for line in result.stdout.decode('utf-8'):
+    result = subprocess.getoutput([cmd.format(artist)])
+    for line in result.split('\n'):
         subprocess.Popen("mpc add {}".format(line),
                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
