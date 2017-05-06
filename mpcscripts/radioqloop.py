f162b8d073ff3e8b8665430db18f324a9f44ac9e
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-05-06 19:54:11 +0200

    experimental radioq loop-script

---
+++ mpcscripts/radioqloop.py
@@ -0,0 +1,37 @@
+import sys
+
+from spottylib import get_seeded_tracks
+from mpclib import get_playlist_files
+
+
+def main():
+    start_index = 0
+    amount = 5
+    if len(sys.argv) > 1:
+        if str(sys.argv[1]).find('h') > -1:
+            print('Gets recommendations from spotify based on tracks from playlist.\n'
+                  'Loops trough the playlist and gets [amount] recommended tracks for every chunk of 5 songs after '
+                  '<start_index>.\n\n'
+                  'usage: python3 radioqloop.py <start_index> <amount>\n\n'
+                  'Optional arguments:\n'
+                  'start_index: where to start getting seed-tracks from the playlist. Default=0.\n'
+                  'amount: how many recommended tracks to get for every five tracks in playlist. CAREFUL WITH THIS. '
+                  'Default=5 min=1 max=100')
+            return
+        start_index = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        amount = int(sys.argv[2])
+
+    tracks = []
+    while tracks:
+        seeds = get_playlist_files(5, start_index)
+        t = get_seeded_tracks(seeds, amount)
+        if not t:
+            break
+        tracks += t
+        start_index += 5
+
+    print('\n'.join(tracks))
+
+if __name__ == '__main__':
+    main()
