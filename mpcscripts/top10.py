ae1cc75dd52ff48e4cb9b27c9491a6da3975f5b9
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-05-06 18:19:00 +0200

    more secure track grep

--- mpcscripts/top10.py
+++ mpcscripts/top10.py
@@ -4,7 +4,7 @@ import sys
 artists = sys.argv[1:]
 for artist in artists:
     artist = str(artist).rstrip('\n')
-    cmd = "mpc -f '%file% splitme%artist%' search artist '{0}' | grep track | grep 'splitme{0}$' | head -10"
+    cmd = "mpc -f '%file% splitme%artist%' search artist '{0}' | grep ':track:' | grep 'splitme{0}$' | head -10"
     result = subprocess.getoutput([cmd.format(artist)])
     for line in result.split('\n'):
         subprocess.Popen("mpc add {}".format(line),
