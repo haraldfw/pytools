44e942dd7619c9d3a63e4e5e5e9f222723166f66
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-05-06 18:44:56 +0200

    this has to stop

--- mpcscripts/radioq.py
+++ mpcscripts/radioq.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     start_index = 0
     amount = 5
     if len(sys.argv) > 1:
-        if str(sys.argv).find('h'):
+        if str(sys.argv[1]).find('h') > -1:
             print('Gets recommendations from spotify based on tracks from playlist.\n\n'
                   'usage: python3 radioq.py <start_index> <amount>\n\n'
                   'start_index: where to start getting seed-tracks from the playlist.\n'
