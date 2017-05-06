e78def42f70fa567ee1f838a9612a1618000ce42
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-05-06 18:43:00 +0200

    help msg

--- mpcscripts/radioq.py
+++ mpcscripts/radioq.py
@@ -6,9 +6,14 @@ if __name__ == '__main__':
     start_index = 0
     amount = 5
     if len(sys.argv) > 1:
+        if sys.argv.find('h'):
+            print('Gets recommendations from spotify based on tracks from playlist.\n\n'
+                  'usage: python3 radioq.py <start_index> <amount>\n\n'
+                  'start_index: where to start getting seed-tracks from the playlist.\n'
+                  'amount: how many tracks from the playlist (starting at start_index) to use as seeds.')
         start_index = int(sys.argv[1])
     if len(sys.argv) > 2:
         amount = int(sys.argv[2])
 
     seeds = get_seeds_from_playlist(start_index, amount)
-    print(get_seeded_tracks(seeds))
+    print('\n'.join(get_seeded_tracks(seeds)))
